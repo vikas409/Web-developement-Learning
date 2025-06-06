f90faf6bd4870bf09f438512eb0f0eae0ba7e7f9
vikas409 <vikasdubey7754@gmail.com>
2025-06-06 16:07:55 +0530

    Project Update
    Signed-off-by: vikas409 <vikasdubey7754@gmail.com>

--- Projects/Counter/script.js
+++ Projects/Counter/script.js
@@ -1,15 +1,33 @@
-let count = 0;
+let count = parseInt(localStorage.getItem("count"))|| 0;
 
     const countDisplay = document.getElementById("count");
     const incrementBtn = document.getElementById("incrementBtn");
     const decrementBtn = document.getElementById("decrementBtn");
+    const resetBtn = document.getElementById("resetBtn");
+    function updateDisplay(){
+      countDisplay.textContent = count;
+      localStorage.setItem("count" , count);
+    }
 
     incrementBtn.addEventListener("click", () => {
-      count++;
-      countDisplay.textContent = count;
+
+      
+        count++;
+        countDisplay.textContent = count;
+      
+      
     });
 
     decrementBtn.addEventListener("click", () => {
-      count--;
-      countDisplay.textContent = count;
+      if (count>0){
+        count--;
+        countDisplay.textContent = count;
+      }
+      
+      
     });
+    resetBtn.addEventListener("click", () =>{
+      count=0;
+      updateDisplay();
+    })  
+updateDisplay();
