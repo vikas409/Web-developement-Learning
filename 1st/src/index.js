c4f41b8e8cea285c55dce6a5f71c6628534dc516
vikas409 <vikasdubey7754@gmail.com>
2025-06-12 16:29:39 +0530

    Update
    Signed-off-by: vikas409 <vikasdubey7754@gmail.com>

--- 1st/src/index.js
+++ 1st/src/index.js
@@ -1,2 +1,19 @@
 import React from "react"
+import ReactDOM from 'react-dom/client'
+function AppOne () {
+    return(
+        <div>AppOne</div>
+    )
+
+
+}
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
+root.render(
+    <React.StrictMode>
+        <AppOne />
+        
+    </React.StrictMode>
+)
 
