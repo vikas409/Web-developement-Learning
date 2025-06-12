046eda99f66ace5f1687ac308a0dc175062e24b1
vikas409 <vikasdubey7754@gmail.com>
2025-06-12 17:01:47 +0530

    Update
    Signed-off-by: vikas409 <vikasdubey7754@gmail.com>

--- React/1st/src/App.js
+++ React/1st/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Card from "./card"
 
 function App() {
   return (
-    <div>
-      
-    </div>
+    <>
+     <div>App</div>
+     <Card />
+    </>
+    
   )
 }
 
---
+++ React/1st/src/card.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function card() {
+  return (
+    <div>
+      This is card
+    </div>
+  )
+}
+
+export default card
